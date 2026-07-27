eb6cbf97a0519f26d4c99d57ff01b041f0be0785
FZ BOY <fzboy2008@gmail.com>
2026-07-27 16:40:22 +0530

    Delete models/BotSettings.js

--- models/BotSettings.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const BotSettingsSchema = new mongoose.Schema({
-    settingKey: { type: String, required: true, unique: true, default: 'global' },
-    coOwners: { type: [String], default: [] } // Array of additional co-owner Discord IDs
-});
-
-module.exports = mongoose.model('BotSettings', BotSettingsSchema);
